b7c73778b2c1377fb4468089032b17808b99a9f8
Nicolas Constant <github@nicolas-constant.com>
2020-03-21 01:16:07 -0400

    testing user controller

---
+++ src/BirdsiteLive/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirdsiteLive.Controllers
+{
+    public class UserController : Controller
+    {
+        [Route("/user")]
+        public IActionResult Index()
+        {
+            var r = Request.Headers["Accept"].First();
+
+            if(r.Contains("application/activity+json"))
+                return Json(new { test = "test" });
+                
+            return View();
+        }
+    }
+}
