55e04331a0adf144775d017ae1053a1336789754
Nicolas Constant <github@nicolas-constant.com>
2020-07-07 01:33:52 -0400

    added worker service

---
+++ src/BirdsiteLive/Services/FederationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BirdsiteLive.Domain;
+using Microsoft.Extensions.Hosting;
+
+namespace BirdsiteLive.Services
+{
+    public class FederationService : BackgroundService
+    {
+        private readonly IUserService _userService;
+
+        #region Ctor
+        public FederationService(IUserService userService)
+        {
+            _userService = userService;
+        }
+        #endregion
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            for (;;)
+            {
+                Console.WriteLine("RUNNING SERVICE");
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+        }
+    }
+}
