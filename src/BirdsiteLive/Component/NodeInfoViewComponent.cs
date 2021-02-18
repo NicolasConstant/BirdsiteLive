67801ea63141955317899ddf99dc06e7d105086d
Nicolas Constant <github@nicolas-constant.com>
2021-02-17 20:49:17 -0500

    added nodeinfo partial view

---
+++ src/BirdsiteLive/Component/NodeInfoViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirdsiteLive.Component
+{
+    public class NodeInfoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(null);
+        }
+    }
+}
