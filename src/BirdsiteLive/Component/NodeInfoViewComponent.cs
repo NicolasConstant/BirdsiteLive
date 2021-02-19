3896afc38045ce81900c8caefab13c68ce985b93
Nicolas Constant <github@nicolas-constant.com>
2021-02-18 21:30:13 -0500

    dynamizing UI

--- src/BirdsiteLive/Component/NodeInfoViewComponent.cs
+++ src/BirdsiteLive/Component/NodeInfoViewComponent.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BirdsiteLive.Domain.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BirdsiteLive.Component
 {
     public class NodeInfoViewComponent : ViewComponent
     {
+        private readonly IModerationRepository _moderationRepository;
+
+        #region Ctor
+        public NodeInfoViewComponent(IModerationRepository moderationRepository)
+        {
+            _moderationRepository = moderationRepository;
+        }
+        #endregion
+
         public IViewComponentResult Invoke()
         {
-            return View(null);
+            var followerPolicy = _moderationRepository.GetModerationType(ModerationEntityTypeEnum.Follower);
+            var twitterAccountPolicy = _moderationRepository.GetModerationType(ModerationEntityTypeEnum.TwitterAccount);
+
+            var viewModel = new NodeInfoViewModel
+            {
+                BlacklistingEnabled = followerPolicy == ModerationTypeEnum.BlackListing ||
+                                      twitterAccountPolicy == ModerationTypeEnum.BlackListing,
+                WhitelistingEnabled = followerPolicy == ModerationTypeEnum.WhiteListing ||
+                                      twitterAccountPolicy == ModerationTypeEnum.WhiteListing,
+                InstanceSaturation = 16,
+            };
+            
+            viewModel = new NodeInfoViewModel
+            {
+                BlacklistingEnabled = false,
+                WhitelistingEnabled = false,
+                InstanceSaturation = 175
+            };
+            return View(viewModel);
         }
+    }
+
+    public class NodeInfoViewModel
+    {
+        public bool BlacklistingEnabled { get; set; }
+        public bool WhitelistingEnabled { get; set; }
+        public int InstanceSaturation { get; set; }
     }
 }
