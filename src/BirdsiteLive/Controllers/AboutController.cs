7abc4a3b3e0f9313ee62d0d269354f10effc506c
Nicolas Constant <github@nicolas-constant.com>
2021-02-22 23:28:24 -0500

    adding about pages and fixing CSS

---
+++ src/BirdsiteLive/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirdsiteLive.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Blacklisting()
+        {
+            return View("Blacklisting");
+        }
+
+        public IActionResult Whitelisting()
+        {
+            return View("Whitelisting");
+        }
+    }
+}
