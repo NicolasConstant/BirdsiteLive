d64063b273f25f5f5d01b41886a0e70068cda8d4
Nicolas Constant <github@nicolas-constant.com>
2022-12-28 16:13:34 -0500

    added federationinfo service

---
+++ src/Tests/BirdsiteLive.Domain.Tests/TheFedInfoServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BirdsiteLive.Domain.Tests
+{
+    [TestClass]
+    public class TheFedInfoServiceTests
+    {
+        [TestMethod]
+        public async Task GetBslInstanceListAsyncTest()
+        {
+            var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            httpClientFactoryMock
+                .Setup(x => x.CreateClient(It.IsAny<string>()))
+                .Returns(new HttpClient());
+
+            var service = new TheFedInfoService(httpClientFactoryMock.Object);
+
+            var bslInstanceList = await service.GetBslInstanceListAsync();
+
+            Assert.IsTrue(bslInstanceList.Count > 0);
+
+            foreach (var instanceInfo in bslInstanceList)
+            {
+                Assert.IsFalse(string.IsNullOrWhiteSpace(instanceInfo.Host));
+                Assert.IsTrue(instanceInfo.Version > new Version(0, 1, 0));
+            }
+        }
+    }
+}
