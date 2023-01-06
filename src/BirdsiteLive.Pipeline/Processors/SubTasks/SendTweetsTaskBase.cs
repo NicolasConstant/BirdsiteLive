5b32a9021f71c5418e583e2ee75ab034d6966c61
Nicolas Constant <github@nicolas-constant.com>
2023-01-05 23:58:58 -0500

    fix CICD

--- src/BirdsiteLive.Pipeline/Processors/SubTasks/SendTweetsTaskBase.cs
+++ src/BirdsiteLive.Pipeline/Processors/SubTasks/SendTweetsTaskBase.cs
@@ -3,29 +3,30 @@ using System.Threading.Tasks;
 using BirdsiteLive.DAL.Contracts;
 using BirdsiteLive.DAL.Models;
 
-namespace BirdsiteLive.Pipeline.Processors.SubTasks;
-
-public class SendTweetsTaskBase
+namespace BirdsiteLive.Pipeline.Processors.SubTasks
 {
-    private readonly ISyncTweetsPostgresDal _syncTweetsPostgresDal;
-
-    #region Ctor
-    protected SendTweetsTaskBase(ISyncTweetsPostgresDal syncTweetsPostgresDal)
+    public class SendTweetsTaskBase
     {
-        _syncTweetsPostgresDal = syncTweetsPostgresDal;
-    }
-    #endregion
+        private readonly ISyncTweetsPostgresDal _syncTweetsPostgresDal;
 
-    protected async Task SaveSyncTweetAsync(string acct, long tweetId, string host, string inbox)
-    {
-        var inboxUrl = $"https://{host}/{inbox.Trim('/')}";
-        var tweet = new SyncTweet
+        #region Ctor
+        protected SendTweetsTaskBase(ISyncTweetsPostgresDal syncTweetsPostgresDal)
         {
-            Acct = acct,
-            TweetId = tweetId,
-            PublishedAt = DateTime.UtcNow,
-            Inbox = inboxUrl
-        };
-        await _syncTweetsPostgresDal.SaveTweetAsync(tweet);
+            _syncTweetsPostgresDal = syncTweetsPostgresDal;
+        }
+        #endregion
+
+        protected async Task SaveSyncTweetAsync(string acct, long tweetId, string host, string inbox)
+        {
+            var inboxUrl = $"https://{host}/{inbox.Trim('/')}";
+            var tweet = new SyncTweet
+            {
+                Acct = acct,
+                TweetId = tweetId,
+                PublishedAt = DateTime.UtcNow,
+                Inbox = inboxUrl
+            };
+            await _syncTweetsPostgresDal.SaveTweetAsync(tweet);
+        }
     }
 }
