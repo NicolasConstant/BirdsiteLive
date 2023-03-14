5b93c2b7607e147a16c0bfb2d8cd838ee89c5152
Nicolas Constant <github@nicolas-constant.com>
2023-03-13 23:50:05 -0400

    added tests

--- src/Tests/BirdsiteLive.Pipeline.Tests/Processors/TweetsCleanUp/RetrieveTweetsToDeleteProcessorTests.cs
+++ src/Tests/BirdsiteLive.Pipeline.Tests/Processors/TweetsCleanUp/RetrieveTweetsToDeleteProcessorTests.cs
@@ -72,5 +72,60 @@ namespace BirdsiteLive.Pipeline.Tests.Processors.TweetsCleanUp
             }
             #endregion
         }
+
+        [TestMethod]
+        public async Task Process_Pagination_Test()
+        {
+            #region Stubs
+            var settings = new InstanceSettings  { };
+            var bufferBlock = new BufferBlock<TweetToDelete>();
+
+
+            var batch = new List<SyncTweet>();
+            var batch2 = new List<SyncTweet>();
+
+            for (var i = 0; i < 100; i++)
+            {
+                var tweet = new SyncTweet
+                {
+                    Id = i
+                };
+                batch.Add(tweet);
+            }
+
+            for (var i = 100; i < 120; i++)
+            {
+                var tweet = new SyncTweet
+                {
+                    Id = i
+                };
+                batch2.Add(tweet);
+            }
+
+            #endregion
+
+            #region Mocks
+            var dalMock = new Mock<ISyncTweetsPostgresDal>(MockBehavior.Strict);
+            dalMock
+                .Setup(x => x.GetTweetsOlderThanAsync(It.IsAny<DateTime>(), It.Is<long>(y => y == -1), It.Is<int>(y => y == 100)))
+                .ReturnsAsync(batch);
+
+            dalMock
+                .Setup(x => x.GetTweetsOlderThanAsync(It.IsAny<DateTime>(), It.Is<long>(y => y == 99), It.Is<int>(y => y == 100)))
+                .ReturnsAsync(batch2);
+            #endregion
+
+            var processor = new RetrieveTweetsToDeleteProcessor(dalMock.Object, settings);
+            processor.GetTweetsAsync(bufferBlock, CancellationToken.None);
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            #region Validations
+            dalMock.VerifyAll();
+
+            Assert.AreEqual(batch.Count + batch2.Count, bufferBlock.Count);
+            #endregion
+        }
+
     }
 }
